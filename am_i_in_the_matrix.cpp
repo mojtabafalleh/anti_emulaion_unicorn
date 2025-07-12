af6babbdedc471ff247c2bacdae23076f985aff0
mojtaba <mojtaba@DESKTOP-57M28DK>
2025-07-12 22:04:10 +0330

    Add project files.

---
+++ am_i_in_the_matrix.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+extern "C" unsigned __int64 MulAndGetFlags(unsigned __int64 input);
+int main() {
+    unsigned __int64 input = 0x12345678;
+    unsigned __int64 flags = MulAndGetFlags(input);
+
+
+    std::cout << "Flags: 0x" << std::hex << flags << std::endl;
+
+    if(flags == 0x206)
+        std::cout << "real PC !";
+    else
+        std::cout << "YOU ARE IN THE MATRIX !";
+    return 0;
+}
